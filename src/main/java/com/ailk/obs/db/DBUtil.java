43ab3244ff0150367d62c3498fbe26f72670c0da
pxuxian <pxuxian@qq.com>
2015-10-26 16:28:56 +0800

    coding

--- src/main/java/com/ailk/obs/db/DBUtil.java
+++ src/main/java/com/ailk/obs/db/DBUtil.java
@@ -1,5 +1,109 @@
 package com.ailk.obs.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.ailk.obs.util.PropertiesUtil;
+
 public class DBUtil {
-	
+	private final static String DBDRIVER = PropertiesUtil.getInstance().get("DB_DRIVER");
+	private final static String DBURL = PropertiesUtil.getInstance().get("DB_URL");
+	private final static String DBUSER = PropertiesUtil.getInstance().get("DB_USERNAME");
+	private final static String DBPASSWORD = PropertiesUtil.getInstance().get("DB_PASSWORD");
+
+	// 获得数据库连接
+	public static Connection getConnection() throws Exception {
+		try {
+			Class.forName(DBDRIVER);
+			Connection conn = DriverManager.getConnection(DBURL, DBUSER, DBPASSWORD);
+			return conn;
+		} catch (Exception e) {
+			throw new Exception("获得数据库连接失败");
+		}
+	}
+
+	public static void close(Connection conn) {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void close(Statement stmt) {
+		if (stmt != null) {
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void close(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/*
+	 * 执行select查询，返回记录总数
+	 */
+	public static int executeSelect(String sql) throws Exception {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = getConnection();
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			int count = 0;
+			while (rs.next()) {
+				count++;
+			}
+			return count;
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			close(rs);
+			close(stmt);
+			close(conn);
+		}
+
+	}
+
+	/*
+	 * insert, update,delete
+	 */
+
+	public static int executeSql(String sql) throws Exception {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = getConnection();
+			stmt = conn.createStatement();
+			return stmt.executeUpdate(sql);
+		} catch (Exception e) {
+			throw e;
+		}finally{
+			close(rs);
+			close(stmt);
+			close(conn);
+		}
+
+	}
+
 }
--- src/main/java/com/ailk/obs/model/User.java
+++ src/main/java/com/ailk/obs/model/User.java
@@ -1,5 +1,85 @@
 package com.ailk.obs.model;
 
+
+//”√ªß¿‡
 public class User {
+	private int userId;
+	private String userName;
+	private String passWd;
+	private int phone;
+	private String mail;
+	private boolean sex;
+	private String summary;
+	private String address;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassWd() {
+		return passWd;
+	}
+
+	public void setPassWd(String passWd) {
+		this.passWd = passWd;
+	}
+
+	public int getPhone() {
+		return phone;
+	}
+
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public boolean isSex() {
+		return sex;
+	}
+
+	public void setSex(boolean sex) {
+		this.sex = sex;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userName=" + userName + ", passWd=" + passWd + ", phone=" + phone
+				+ ", mail=" + mail + ", sex=" + sex + ", summary=" + summary + ", address=" + address + "]";
+	}
 
 }
